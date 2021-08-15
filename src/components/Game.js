271a0d4110f5a249dfd1f60cb33ba5e70e452155
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-16 02:16:27 +0300

    Implement Time Travel

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,34 +4,48 @@ import Board from './Board';
 
 const style = {
   width: '200px',
-  margin: '20px auto'
+  margin: '20px auto',
 };
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+
   const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = (i) => {
-    const boardCopy = [...board];
-    if (winner || boardCopy[i]) return;
-    boardCopy[i] = xIsNext ? 'x' : 'o';
-    setBoard(boardCopy);
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
+    if (winner || squares[i]) return;
+    squares[i] = xIsNext ? 'x' : 'o';
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
     setXIsNext(!xIsNext);
   };
-  const jumpTo = () => {};
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
 
-  const renderMoves = () => (
-    <button onClick={() => setBoard(Array(9).fill(null))}>
-      Start Game
-    </button>
-  );
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move ${move}` : 'Go to start';
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>
+            {destination}
+          </button>
+        </li>
+      );
+    });
 
   return (
     <div>
-      <Board squares={board} onClick={handleClick} />
+      <Board squares={history[stepNumber]} onClick={handleClick} />
       <div style={style}>
-        <p style={{display: 'flex', justifyContent: 'space-between'}}>
+        <p >
           {winner
             ? 'Winner: ' + winner
             : 'Next Player: ' + (xIsNext ? 'x' : 'o')}
