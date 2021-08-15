8cfe15381886b0553a190481cdd1506274fabf92
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-16 01:41:51 +0300

    renderMoves function and the last JSX

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { calculateWinner } from '../helpers';
 import Board from './Board';
 
+const style = {
+  width: '200px',
+  margin: '20px auto'
+};
+
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
@@ -13,18 +18,28 @@ const Game = () => {
     boardCopy[i] = xIsNext ? 'x' : 'o';
     setBoard(boardCopy);
     setXIsNext(!xIsNext);
-  }
-  const jumpTo = () => {
+  };
+  const jumpTo = () => {};
 
-  }
-
-  const renderMoves = () => {
-
-  }
+  const renderMoves = () => (
+    <button onClick={() => setBoard(Array(9).fill(null))}>
+      Start Game
+    </button>
+  );
 
   return (
-    <Board squares={board} onClick={handleClick} />
-  )
-}
+    <div>
+      <Board squares={board} onClick={handleClick} />
+      <div style={style}>
+        <p style={{display: 'flex', justifyContent: 'space-between'}}>
+          {winner
+            ? 'Winner: ' + winner
+            : 'Next Player: ' + (xIsNext ? 'x' : 'o')}
+          {renderMoves()}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Game;
